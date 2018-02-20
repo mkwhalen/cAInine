410fb514bdc6175cdd8efcab3310223cffb51900
SuavePirate <alexander.dunn@outlook.com>
2018-02-19 23:00:19 -0500

    Initial projects

---
+++ src/Apps/Application/CAInine.Apps.Application.Bootstrap/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CAInine.Apps.Application.Bootstrap
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Cloud/Tests/CAInine.Tests.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CAInine.Tests.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
