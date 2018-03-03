d6d43d705a716018e7e748c1f2b22d5b7f04eec9
alexander.dunn@outlook.com <alexander.dunn@outlook.com>
2018-03-03 12:45:08 -0800

    added auto generated types from petfinder's xsd file

---
+++ src/Cloud/Core/CAInine.Core.Models/Configuration/PetFinder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CAInine.Core.Models.Configuration
+{
+    /// <summary>
+    /// Configuration manager for our pet finder api values
+    /// </summary>
+    public class PetFinder
+    {
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
