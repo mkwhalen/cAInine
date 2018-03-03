49a9e06a3ecc9f299d0c473aa0ad796781f84d92
alexander.dunn@outlook.com <alexander.dunn@outlook.com>
2018-03-03 12:57:21 -0800

    Updated models and created provider interface

---
+++ src/Cloud/Core/CAInine.Core.Interfaces/Providers/IPetSearchProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using CAInine.Core.Models.Transfer.PetFinder;
+
+namespace CAInine.Core.Interfaces.Providers
+{
+    /// <summary>
+    /// Pet search provider. Allows for finding breeds, shelters, and pets that are available for adoption
+    /// </summary>
+    public interface IPetSearchProvider
+    {
+        /// <summary>
+        /// Gets the available breeds async.
+        /// </summary>
+        /// <returns>The available breeds async.</returns>
+        /// <param name="animal">Animal.</param>
+        Task<petfinderBreedList> GetAvailableBreedsAsync(string animal);
+
+        /// <summary>
+        /// Gets the random pet async.
+        /// </summary>
+        /// <returns>The random pet async.</returns>
+        /// <param name="animal">Animal.</param>
+        Task<petfinderPetRecord> GetRandomPetAsync(string animal);
+
+        /// <summary>
+        /// Gets the pets by breed async.
+        /// </summary>
+        /// <returns>The pets by breed async.</returns>
+        /// <param name="animal">Animal.</param>
+        /// <param name="breed">Breed.</param>
+        /// <param name="location">Either Zip or City,State.</param>
+        Task<petfinderPetRecordList> GetPetsByBreedAsync(string animal, string breed, string location);
+
+        /// <summary>
+        /// Gets the shelters by breeds they have available.
+        /// </summary>
+        /// <returns>The shelters by breed.</returns>
+        /// <param name="animal">Animal.</param>
+        /// <param name="breed">Breed.</param>
+        /// <param name="skip">Skip.</param>
+        /// <param name="take">Take.</param>
+        Task<petfinderShelterRecordList> GetSheltersByBreed(string animal, string breed, int skip, int take);
+
+        /// <summary>
+        /// Gets the shelters by location.
+        /// </summary>
+        /// <returns>The shelters by location.</returns>
+        /// <param name="location">Either zip or City,State.</param>
+        Task<petfinderShelterRecordList> GetSheltersByLocation(string location);
+
+        /// <summary>
+        /// Gets the pets at shelter.
+        /// </summary>
+        /// <returns>The pets at shelter.</returns>
+        /// <param name="shelterId">Shelter identifier.</param>
+        Task<petfinderPetRecordList> GetPetsAtShelter(string shelterId);
+    }
+}
---
+++ src/Cloud/Infrastructure/CAInine.Infrastructure.Data/Providers/PetFinderDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CAInine.Infrastructure.Data.Providers
+{
+    /// <summary>
+    /// Data provider for communicating the the petfinder.com api
+    /// </summary>
+    public class PetFinderDataProvider
+    {
+        public PetFinderDataProvider()
+        {
+        }
+    }
+}
